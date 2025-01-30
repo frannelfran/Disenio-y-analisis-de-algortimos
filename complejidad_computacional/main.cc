cd94faf1ba839c1bad3c50e154c602a48c99b48d
alu0101571669 <alu0101571669@ull.edu.es>
2025-01-30 22:53:04 +0000

    Programa principal

--- complejidad_computacional/main.cc
+++ complejidad_computacional/main.cc
@@ -8,26 +8,41 @@
  * @brief Programa que realiza la multiplicación de matrices
 */
 
-int main() {
-  vector<vector<int>> matriz1 = generarMatriz(3, 3);
-  vector<vector<int>> matriz2 = generarMatriz(2, 3);
+int main(int argc, char* argv[]) {
+  Tools datos = leerDatos(argc, argv);
+  // Genero las matrices con los datos de entrada
+  vector<vector<int>> matriz1 = generarMatriz(datos.columnas1, datos.filas1);
+  vector<vector<int>> matriz2 = generarMatriz(datos.columnas2, datos.filas2);
 
-  Multiplicacion* multiplicacion = new MultiplicacionFilas(matriz1, matriz2);
+  // Creo los objetos de las clases
+  Multiplicacion* multi_filas = new MultiplicacionFilas(matriz1, matriz2);
+  Multiplicacion* multi_columnas = new MultiplicacionColumnas(matriz1, matriz2);
+
+  // Calculo el tiempo de ejecución de la multiplicación por filas
+  auto start = chrono::high_resolution_clock::now();
+  vector<vector<int>> resultado_filas = multi_filas->multiplicar();
+  auto end = chrono::high_resolution_clock::now();
+  auto duracion_filas = chrono::duration_cast<chrono::microseconds>(end - start);
+
+  // Calculo el tiempo de ejecución de la multiplicación por columnas
+  start = chrono::high_resolution_clock::now();
+  vector<vector<int>> resultado_columnas = multi_columnas->multiplicar();
+  end = chrono::high_resolution_clock::now();
+  auto duracion_columnas = chrono::duration_cast<chrono::microseconds>(end - start);
+
+  // Muestro los resultados
   cout << "--- Matriz 1 ---" << endl;
   mostrarMatriz(matriz1);
   cout << "--- Matriz 2 ---" << endl;
   mostrarMatriz(matriz2);
-  mostrarMatriz(multiplicacion->multiplicar());
+  cout << "--- Resultado multiplicación por filas ---" << endl;
+  mostrarMatriz(resultado_filas);
+  cout << "--- Resultado multiplicación por columnas ---" << endl;
+  mostrarMatriz(resultado_columnas);
+  cout << "Tiempo de ejecución multiplicación por filas: " << duracion_filas.count() << " microsegundos" << endl;
+  cout << "Tiempo de ejecución multiplicación por columnas: " << duracion_columnas.count() << " microsegundos" << endl;
+  delete multi_filas;
+  delete multi_columnas;
   
-  
-  
-
-
-
-
-
-
-
-
-
+  return 0;
 }
