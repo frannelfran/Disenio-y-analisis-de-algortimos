ca2bf5652b4381a857bb648104bce3f580ebb210
alu0101571669 <alu0101571669@ull.edu.es>
2025-01-30 18:37:28 +0000

    Declaración de la función para generar matrices

---
+++ complejidad_computacional/tools/tools.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <random> // Para generar los números aleatorios
+#include <vector> // Para usar matrices
+
+using namespace std;
+
+vector<vector<int>> generarMatriz(int filas, int columnas);
